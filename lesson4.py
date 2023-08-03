427176b806fb323a7cd4eda458d22e386157332b
popovych2010 <o.popovych2010@gmail.com>
2023-08-03 21:41:42 +0200

    intro strings

--- lesson4.py
+++ lesson4.py
@@ -2,5 +2,7 @@ some_string = 'Смажену картоплЮ'.lower()
 
 smiling_face_with_heart_shaped_eyes = ' \U0001F60D'
 
+some_string = input('Enter your eat >>>')
+
 sentence = f"О, я теж люблю {some_string}{smiling_face_with_heart_shaped_eyes}"
 print(sentence)
