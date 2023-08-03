60fff418e422f5334ae918ba3c7446fc6ee5b35b
popovych2010 <o.popovych2010@gmail.com>
2023-08-03 18:36:27 +0200

    intro strings

---
+++ lesson4.py
@@ -0,0 +1,6 @@
+some_string = 'Смажену картоплЮ'.lower()
+
+smiling_face_with_heart_shaped_eyes = ' \U0001F60D'
+
+sentence = f"О, я теж люблю {some_string}{smiling_face_with_heart_shaped_eyes}"
+print(sentence)
