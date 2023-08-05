0ede0211fd3b72a24c0afa41b04c0b4688118094
popovych2010 <o.popovych2010@gmail.com>
2023-08-05 18:12:09 +0200

    intro strings

---
+++ lesson6.py
@@ -0,0 +1,10 @@
+user_age = ('Enter your  age : ')
+#print(type(user_age))
+print(user_age)
+user_age = float(user_age_str_format)   user_age_float: 5.5
+
+
+
+user_age_back_str = str (user_age)
+print(type(user_age))
+print(user_age * 2)
